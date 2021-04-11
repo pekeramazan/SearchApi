ee57f7f802c81e8f2041fc06d3d11c258d17681e
ramazan peker <ramazan.peker@nextlua.com>
2021-04-11 09:53:44 +0300

    first

---
+++ app/src/main/java/com/example/searchapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.searchapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//
